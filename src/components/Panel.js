90500ee541a19b0284f5cd0aa57f11aef14b4691
Rawan <r.k096@hotmail.com>
2024-04-14 12:32:08 +0300

    panels with ref

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -8,52 +8,62 @@ gsap.registerPlugin(ScrollTrigger);
 
 function Panels() {
   const app = useRef();
+  const orangie = useRef();
+  const bluie = useRef();
+  const pinkie = useRef();
 
-  useLayoutEffect(() => {
-    const ctx = gsap.context((self) => {
-      const panels = self.selector(".animate-panel");
-      gsap.set(panels, { xPercent: 100, opacity: 1 });
+  useLayoutEffect(
+    () => {
+      // const ctx = gsap.context((self) => {
+      //   const panels = self.selector(".animate-panel");
+      //   gsap.set(panels, { yPercent: 100, opacity: 1 });
+      //   const tl = gsap.timeline();
+      //   panels.forEach((panel, i) => {
+      //     tl.to(panel, {
+      //       yPercent: 0,
+      //       ease: "none"
+      //     });
+      //   });
+      //   ScrollTrigger.create({
+      //     animation: tl,
+      //     trigger: app.current,
+      //     start: "top top",
+      //     end: () => "+=" + 100 * panels.length + "%",
+      //     pin: true,
+
+      //     scrub: 0.5,
+      //     snap: {
+      //       snapTo: 1 / panels.length,
+      //       duration: 0.25,
+      //       ease: "power2.out",
+      //       delay: 0
+      //     }
+      //   });
+      // }, app);
+      // return () => ctx.revert();
+
       const tl = gsap.timeline();
-      panels.forEach((panel, i) => {
-        i % 2 == 0 ? 
-        tl.to(panel, {
-          xPercent: 0,
-          ease: "none"
-        }) :
-        tl.to(panel, {
-            xPercent: -100,
-            ease: "none"
-          })
-        ;
-      });
-      ScrollTrigger.create({
-        animation: tl,
-        trigger: app.current,
-        start: "top top",
-        end: () => "+=" + 100 * panels.length + "%",
-        pin: true,
-        
-        scrub: 0.5,
-        snap: {
-          snapTo: 1 / panels.length,
-          duration: 0.25,
-          ease: "power2.out",
-          delay: 0
-        }
-      });
-    }, app);
-    return () => ctx.revert();
-  }, []);
+      tl.from(orangie.current, { xPercent: -100 })
+        .from(bluie.current, { xPercent: 100 })
+        .from(pinkie.current, { yPercent: -100 });
+   
 
+    ScrollTrigger.create({
+      animation: tl,
+      trigger: app.current,
+      start: "top top",
+      end: "+=4000",
+      scrub: true,
+      pin: true,
+      anticipatePin: 1,
+    });
+}, []);
   return (
     <div className="panels-container" ref={app}>
-      <section className="panel">
-      one
-      </section>
-      <section className="panel animate-panel pirplie">ONE</section>
-      <section className="panel animate-panel orangie">TWO</section>
-      <section className="panel animate-panel bluie">THREE</section>
-      <section className="panel animate-panel pinkie">FOUR</section>
+      <section className="panel">one</section>
+      <section className="panel animate-panel orangie" ref={orangie}>TWO</section>
+      <section className="panel animate-panel bluie" ref={bluie}>THREE</section>
+      <section className="panel animate-panel pinkie" ref={pinkie}>FOUR</section>
     </div>
   );
 }
