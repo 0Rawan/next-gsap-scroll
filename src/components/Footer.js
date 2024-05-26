fe608cbae66fc086b0b787de74b63d76633a8194
Rawan <r.k096@hotmail.com>
2024-05-26 19:36:12 +0300

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,32 @@
 import React from "react";
+import { FaGithub, FaFile  } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
 function Footer() {
   return (
-    <footer id='footer' className="footer">
-       <div className="work-container">
-              <p className='work-title'>What &apos;s the next step?</p>
-              <p className='work-description'> Contact me to continue the journey of creativeness</p>
-          </div>
+    <footer id="footer" className="footer">
+      <div className="work-container">
+        <p className="work-title">What's next?</p>
+        <p className="work-description">//Let's contact, for the next step</p>
+      </div>
       <div class="footer-container">
-  <a href="https://www.linkedin.com/in/rawan-mustafa-087725140/" class="btn-1">linkedin</a>
-  <a href="https://github.com/0Rawan" class="btn-2">Github</a>
-  <a href="mailto:00rawan.kamal@gmail.com" class="btn-3">Email</a>
-  <a href="assets/RawanMustafaResume.pdf" download  class="btn-4">Resume</a>
-</div>
+        <a
+          href="https://www.linkedin.com/in/rawan-mustafa-087725140/"
+          class="btn-1"
+        >
+          <FaLinkedin /> linkedin
+        </a>
+        <a href="https://github.com/0Rawan" class="btn-2">
+        <FaGithub /> Github
+        </a>
+        <a href="mailto:00rawan.kamal@gmail.com" class="btn-3">
+        <MdEmail /> Email
+        </a>
+        <a href="assets/RawanMustafaResume.pdf" download class="btn-4">
+        <FaFile />  Resume
+        </a>
+      </div>
     </footer>
   );
 }
